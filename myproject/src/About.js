d5ea820dfc98a670efed4e15ec1aade3cbe8b22d
ShreyaBharde <bhardeshreya19@gmil.com>
2023-09-11 10:19:44 +0530

    first commit

---
+++ myproject/src/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css'
+
+class About extends React.Component {
+    render() {
+        return <>
+
+            <div>
+                <h1>My fourth heading</h1>
+            </div>
+            <About1/>
+        </>
+    }
+}
+class About1 extends React.Component {
+    render() {
+        return <>
+{/* 
+            <div>
+                <h1>My fifth heading</h1>
+            </div> */}
+        </>
+    }
+}
+export default About;
---
+++ myproject/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+
+  return (
+    <>
+	<h1>rycfjrgfuyrfngerj</h1>
+    </>
+  );
+}
+export default App();
